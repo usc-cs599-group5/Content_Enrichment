0a3a6d728276955ab729fe5d28858e4b269fecd3
Andrew Downing <ad510n1@gmail.com>
2016-04-02 23:11:53 -0700

    solr ingestion for DOI and GeoTopicParser

---
+++ solr_ingest.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""
+setup before running this:
+pip3 install --user SolrClient
+download and extract solr 4.10 from https://archive.apache.org/dist/lucene/solr/4.10.4/solr-4.10.4.tgz
+copy schema.xml to solr-4.10.4/example/solr/collection1/conf
+bin/solr start
+
+after running this script: http://localhost:8983/solr -> core admin -> reload
+
+schema docs: https://cwiki.apache.org/confluence/display/solr/Documents%2C+Fields%2C+and+Schema+Design
+clear solr index: https://wiki.apache.org/solr/FAQ#How_can_I_delete_all_documents_from_my_index.3F
+"""
+
+import json
+from collections import defaultdict
+from SolrClient import SolrClient
+
+solr = SolrClient('http://localhost:8983/solr')
+j = defaultdict(dict)
+with open('DOI.json', 'r') as f:
+    for k, v in json.load(f).items():
+        j[k]['doi'] = v
+with open('geotopic.json', 'r') as f:
+    for k, v in json.load(f).items():
+        j[k].update(v)
+for k, v in j.items():
+    v['id'] = k
+#print(json.dumps(list(j.values())))
+solr.index_json('collection1', json.dumps(list(j.values())))
