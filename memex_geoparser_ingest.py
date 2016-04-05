1ada5c0c520c5d041038221f0be7960b31fff092
Andrew Downing <ad510n1@gmail.com>
2016-04-05 03:58:33 -0700

    partition geoparser output across multiple files

--- memex_geoparser_ingest.py
+++ memex_geoparser_ingest.py
@@ -2,17 +2,21 @@
 
 import json
 import sys
-from SolrClient import SolrClient
+
+partition = 200
 
 if len(sys.argv) < 2:
-    print("usage: ./memex_geoparser_ingest.py /path/to/GeoParser filename")
+    print("usage: ./memex_geoparser_ingest.py /path/to/GeoParser")
     sys.exit()
+# extract set of unique locations from geotopic.json
 locations = set()
 with open('geotopic.json', 'r') as f:
     for j in json.load(f).values():
         for k, v in j.items():
             if 'NAME' in k:
                 locations.add(v)
-        if len(locations) >= 200: break
-with open(sys.argv[1] + '/geoparser_app/static/uploaded_files/' + sys.argv[2], 'w') as f:
-    f.write('\n'.join(locations))
+# partition into several files because GeoParser chokes on large files
+locations = list(locations)
+for i in range(len(locations) // partition):
+    with open(sys.argv[1] + '/geoparser_app/static/uploaded_files/' + str(i), 'w') as f:
+        f.write('\n'.join(locations[i*partition : (i+1)*partition]))
